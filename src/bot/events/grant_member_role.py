61880140968e018b0a83716e504a5f75d38b414c
Newspaper8459 <mmisaizu53@gmail.com>
2026-05-08 20:19:07 +0900

    conditionals

--- src/bot/events/grant_member_role.py
+++ src/bot/events/grant_member_role.py
@@ -76,8 +76,12 @@ class GrantMemberRole(commands.Cog):
       channel = guild.get_channel(user.channel_id)
       assert isinstance(channel, discord.TextChannel)
 
-      if user.grade in {'b5', 'b6', 'm1', 'm2', 'd'}:
-        category_name = 'TIMES B5/B6/M/D'
+      if user.grade in {'b5', 'm1'}:
+        category_name = 'TIMES B5/M1'
+      elif user.grade in {'b6', 'm2'}:
+        category_name = 'TIMES B6/M2'
+      elif user.grade == 'd':
+        category_name = 'TIMES D'
       elif user.grade == 'other':
         category_name = 'TIMES other'
       else:
